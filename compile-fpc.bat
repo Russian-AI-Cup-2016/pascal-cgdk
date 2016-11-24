6b7c8e62e941f3dbce47b0c8b6cca7e9beb5ce10
SladeThe <sladethe@gmail.com>
2016-11-25 00:08:51 +0300

    Improved compile-fpc.bat.

--- compile-fpc.bat
+++ compile-fpc.bat
@@ -5,9 +5,17 @@ if not exist %name%.pas (
     exit 1
 )
 
-del /F /Q %name%.exe
+del /F /Q %name%.exe *.ppu *.o *.dcu *.pcu
 
-fpc.exe -Cs67107839 -Mdelphi -XS -vm3018 Runner.dpr -o%name%.exe 1>compilation.log
+set COMPILER_PATH="
+
+if "%FREEPASCAL_HOME%" neq "" (
+    if exist "%FREEPASCAL_HOME%\bin\i386-win32\fpc.exe" (
+        set COMPILER_PATH="%FREEPASCAL_HOME%\bin\i386-win32\"
+    )
+)
+
+call "%COMPILER_PATH:"=%fpc.exe" -Cs67107839 -Mdelphi -XS -vm3018 Runner.dpr -o%name%.exe 1>compilation.log
 
 if exist Runner.exe (
     move /Y Runner.exe %name%.exe
